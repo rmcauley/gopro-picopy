d4f88cfcb4fa85b79cefcae0e8c506508848a123
Rob McAuley <rmcauley@gmail.com>
2020-06-23 21:34:37 +0900

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # gopro-picopy
- Copies GoPro/DCIM movies and images to a larger drive on a Raspberry Pi.
+
+Copies GoPro movies and images to the largest attached drive.  Meant to be used on Raspberry Pis as a lightweight portable method of backing up MicroSD cards used on GoPros to larger disks for those longer trips when you don't want to haul a laptop to do the job, or if you're wary of paying more for all-in-one devices.
+
+## What does it do?
+
+- Finds the smallest mounted disk that has a `DCIM/GOPRO100` directory
+- Starts copying MP4, JPG, and JPEG files to the largest connected drive (not the internal MicroSD card)
+- SHA256 verifies each file after copying for integrity
+
+## What doesn't it do?
+
+- Run automatically on device mount (you will need a way to start the script on the Pi yourself)
+- Delete files (format the card on your GoPro once satisfied with the copy results)
+- Show file copy progress
---
+++ gopro-picopy.py
@@ -0,0 +1,60 @@
+import os
+import psutil
+import datetime
+import shutil
+import hashlib
+from os import path
+
+IGNORE_MOUNTPOINTS = ("/", "/boot")
+COPY_FILENAMES = (".mp4", ".jpg", ".jpeg")
+
+# taken from:  https://stackoverflow.com/a/44873382
+def sha256sum(filename):
+    h  = hashlib.sha256()
+    b  = bytearray(128*1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        for n in iter(lambda : f.readinto(mv), 0):
+            h.update(mv[:n])
+    return h.hexdigest()
+
+drives = sorted(
+    [
+        (p.mountpoint, psutil.disk_usage(p.mountpoint).total)
+        for p in psutil.disk_partitions()
+        if p.mountpoint not in IGNORE_MOUNTPOINTS
+    ],
+    key=lambda p: p[1],
+)
+
+source = os.path.join(next(
+    (
+        drive[0]
+        for drive in drives
+        if path.exists(path.join(drive[0], "DCIM", "100GOPRO"))
+    ),
+    None,
+), "DCIM", "100GOPRO")
+destination = path.join(drives[-1][0], datetime.datetime.now().strftime("%Y-%m-%d"))
+
+try:
+    os.mkdir(destination)
+except FileExistsError:
+    pass
+
+print(f"Will copy from {source} to {destination}")
+
+for _, _, files in os.walk(source):
+    for filename in files:
+        full_source = os.path.join(source, filename)
+        full_destination = os.path.join(destination, filename)
+        if filename.lower().split(".")[-1] in COPY_FILENAMES:
+            source_sha = sha256sum(full_source)
+            print(f"Copying {filename}")
+            # copy2 preserves metadata
+            shutil.copy2(full_source, full_destination)
+            destination_sha = sha256sum(full_destination)
+            if source_sha != destination_sha:
+                raise ValueError(f"{filename} hash is different between source and destination.")
+
+print("All done.")
